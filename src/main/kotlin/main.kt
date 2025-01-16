ef3870a28d5f545f1147d59f026ba81cd722c2ee
Artiom test <art3937@gmail.com>
2025-01-16 14:24:04 +0300

    add test

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,14 +3,14 @@ fun main() {
     ChatService.addMessage(1, Message("by"))
     ChatService.addMessage(2, Message("hello2"))
     ChatService.addMessage(3, Message("help"))
-    println(ChatService.getChatMessage(1))
-    println(ChatService.getChatMessage(2))
-    println(ChatService.deleteMessage(0))
-    println(ChatService.deleteMessage(1))
+//    println(ChatService.getChatMessage(1))
+//    println(ChatService.getChatMessage(2))
+//    println(ChatService.deleteMessage(0))
+//    println(ChatService.deleteMessage(1))
     println(ChatService.lastMessage())
-    println(ChatService.getUnreadChatsCount())
-    println(ChatService.deleteChat(3))
-    ChatService.getChats()
+//    println(ChatService.getUnreadChatsCount())
+//    println(ChatService.deleteChat(3))
+//    ChatService.getChats()
 }
 
 object ChatService {
